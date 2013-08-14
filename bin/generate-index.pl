22980e7eb2d7a40ee0ab0433ae0b9cd1fb5ecf71
Kang-min Liu <gugod@gugod.org>
2013-08-14 09:05:17 +0200

    an index generating program. Closes #1

---
+++ bin/generate-index.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/env perl
+use v5.14;
+use strict;
+use FindBin;
+use JSON::PP;
+use Getopt::Std;
+
+my %opts;
+getopts("o:", \%opts);
+
+my $json = JSON::PP->new->utf8;
+
+my @entries;
+local $/ = undef;
+for my $config (<${FindBin::Bin}/../etc/*.json>) {
+    my ($frequency) = $config =~ m!/([^/]+)\.json$!;
+
+    open my $fh, "<", $config;
+    my $sites = $json->decode(<$fh>);
+    for my $site (@$sites) {
+        my $base = "http://g0v-data.github.io/mirror/";
+        push @entries, [
+            $site->{name},
+            $frequency,
+            $site->{url},
+            $base . $site->{output},
+            $site->{process} ? $base . $site->{process}{output} : "",
+        ];
+    }
+}
+
+use utf8;
+my $html = "";
+for (@entries) {
+    $html .= join "",
+        "<h3>$_->[0] $_->[1]</h3>",
+        "<ul>",
+        qq{<li>原址： <a href="$_->[1]">$_->[2]</a></li>},
+        qq{<li>備份： <a href="$_->[2]">$_->[3]</a></li>},
+        qq{<li>處理： <a href="$_->[1]">$_->[4]</a></li>},
+        "</ul>\n";
+}
+
+$html = <<"HTML";
+<html>
+    <head><title>g0v-data mirror</title></head>
+    <body>$html</body>
+</html>
+HTML
+
+if ($opts{o}) {
+    open my $fh, ">:utf8", $opts{o};
+    say $fh $html;
+}
+else {
+    binmode STDOUT, ":utf8";
+    say $html;
+}
