d5c9cfca6c453155484bf8c6d2f5c7bf2087502f
Kang-min Liu <gugod@gugod.org>
2016-09-03 23:55:31 +0200

    check and errors.

--- bin/check-official-data-list.pl
+++ bin/check-official-data-list.pl
@@ -7,6 +7,17 @@ use URI;
 use JSON::PP;
 use File::Slurp qw(read_file);
 use Encode qw(encode_utf8 decode_utf8);
+use HTTP::Tiny;
+
+my $JSON = JSON::PP->new->utf8->canonical;
+
+sub report_error {
+    my ($error) = @_;
+    if ( $error->{response} ) {
+        delete $error->{response}{content};
+    }
+    say STDERR $JSON->encode($error);    
+}
 
 my $input = $ARGV[0] or exit(1);
 
@@ -23,21 +34,22 @@ for (my $i = 0; $i < @$records; $i++) {
     my $url = encode_utf8($record->{$kDownloadlink});
     my $u = URI->new($url);
 
-    if ($u->scheme) {
-        $seen{$url} = 1;
-    } else {
-        push @errors, {
-            reason => "undefined uri scheme",
-            record => $record,
+    my $uri_scheme = $u->scheme;
+    if ($uri_scheme) {
+        if ($uri_scheme =~ /\A https? \z/x) {
+            $seen{$url} = $record;
+        } else {
+            report_error({ error => "Unsupported URI scheme.",  record => $record });
         }
+    } else {
+        report_error({ error => "undefined uri scheme.",  record => $record });
     }
 }
 
-my $JSON = JSON::PP->new->utf8->canonical;
-for (@errors) {
-    say $JSON->encode($_);
+for my $url (sort { $a cmp $b } keys %seen) {
+    my $http = HTTP::Tiny->new;
+    my $response = $http->get($url);
+    unless ( $response->{success} ) {
+        report_error({ error => "Non-successful http response.",  record => $seen{$url}, response => $response });
+    }
 }
-
-# for my $url (sort { $a cmp $b } keys %seen) {
-#     say $url;
-# }
