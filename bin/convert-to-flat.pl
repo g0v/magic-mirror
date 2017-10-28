a12b9bdf6f0bac391b577358482ca856e840c7f4
Kang-min Liu <gugod@gugod.org>
2017-10-29 05:39:16 +0800

    do some 'git commit' in the middle of progress.
    A massive git commit will lead to suffering when we
    are pushing to a remote.

--- bin/convert-to-flat.pl
+++ bin/convert-to-flat.pl
@@ -31,13 +31,28 @@ chdir $mirror_repo;
 
 my $forkman = Parallel::ForkManager->new(2);
 
+my $ABORT = 0;
+$forkman->run_on_finish(
+    sub {
+        my ($exit_status, $pid) = @_;
+        if ($exit_status != 0) {
+            $ABORT = 1;
+        }
+    }
+);
+
 for my $dataset (@dataset) {
+    last if $ABORT;
+
     say ">> $dataset->{filename}";
     $forkman->start and next;
 
     my $cmd = "git log --format='%H %at' $dataset->{filename}";
     open my $fh, "-|", $cmd;
+
     while (<$fh>) {
+        last if $ABORT;
+
         chomp;
         my ($sha1, $author_timestamp) = split / /, $_, 2;
         my @t = (gmtime($author_timestamp))[5,4,3,2,1];
@@ -60,6 +75,25 @@ for my $dataset (@dataset) {
         say ">> $flat_file_name";
         make_path($flat_dir) unless -d $flat_dir;
         system("git show ${sha1}:$dataset->{filename} > $flat_file_name");
+
+        state $prev_flat_dir;
+        if (defined($prev_flat_dir) && $flat_dir ne $prev_flat_dir) {
+            my $LOCK = "/tmp/gitlock";
+            while(-f $LOCK) {
+                sleep(1);
+            }
+
+            open(my $fh_lock, ">", $LOCK) or die $!;
+            chdir($flat_mirror_repo);
+            my $rc = system(qw(git add), $prev_flat_dir);
+            if ($rc == 0) {
+                $rc = system(qw(git commit -m flatten));
+            }
+            chdir($mirror_repo);
+            close($fh_lock);
+            unlink($LOCK);
+        }
+        $prev_flat_dir = $flat_dir;
     }
     $forkman->finish;
 }
